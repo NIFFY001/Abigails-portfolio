86da8afe0c633e11afb459f9b3936c97e6b0bc79
Akinsanmi Victor <victorakinsanmi2018@gmail.com>
2024-08-07 10:04:50 +0100

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,14 +18,18 @@ import hire1 from "../assets/hire1.png";
 import cv from "../assets/cv.png";
 import slider from "../assets/slider.png";
 import abigail2 from "../assets/abigail2.png";
+import aboutimg from "../assets/aboutimg.png";
+import instagram from "../assets/instagram.png";
+import tiktok from "../assets/tiktok.png";
+import linkedin from "../assets/linkedin.png";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <div className="bg-[#161513] min-h-[100vh] p-5 md:p-20 w-full">
-        <div className="flex flex-col md:flex-row gap-5 leading-[50px] md:mb-[17px] mb-[-30px]">
-          <div className="w-full">
+        <div className="flex flex-col md:flex-row gap-5 md:leading-[60px] leading-[30px] md:mb-[17px] mb-[30px] items-center">
+          <div className="w-full animation-slide-in-left">
             <h1 className="text-white flex items-center gap-3 text-3xl pb-5">
               Hi! <img src={hand} className="h-[40px]" alt="hand" />
             </h1>
@@ -45,7 +49,7 @@ export default function Home() {
               </div>
             </div>
             <div className="text-white pb-5">
-              <p>
+              <p className=" md:text-[20px]">
                 I design the designerable, I have design skills and prototyping
                 skills, looking for that pretty designer she is right here.
               </p>
@@ -56,15 +60,25 @@ export default function Home() {
               <img src={view} alt="view" />
             </div>
 
-            <div className="text-white">
+            <div className="text-white ">
               <p className="pb-5 text-xl">Follow Me</p>
-              <img src={socials} alt="socials" />
+              <div className="flex gap-4">
+                <a href="https://www.linkedin.com/in/alabi-abigail-18495423a/">
+                  <img src={linkedin} alt="linkedin" />
+                </a>
+                <a href="https://www.tiktok.com/@abigail_tastes">
+                  <img src={tiktok} alt="tiktok" />
+                </a>
+                <a href="https://www.instagram.com/abigailalabi0/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+              </div>
             </div>
           </div>
 
-          <div className="w-full bg-[#161513]">
+          <div className="w-full bg-[#161513] animation-slide-in-right">
             <video
-              className="bg-[#161513] h-[500px] w-full"
+              className="bg-[#161513] md:h-[700px] w-full"
               src={hero2}
               autoPlay
               loop
@@ -76,24 +90,24 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mb-[20px]">
+        <div className="md:mb-[50px] mb-[20px]">
           <FullScroller image={slider} />
         </div>
 
         <div className="text-white flex  flex-col md:flex-row md:w-[100%] gap-5 items-center">
-          <div className="md:w-[50%] w-full">
+          <div className="md:w-[50%] animation-slide-in-left">
             <img src={abigail2} className="h-full w-full object-cover"></img>
           </div>
 
-          <div className="md:w-[50%] ">
+          <div className="md:w-[50%] mb-8 animation-slide-in-right ">
             <div className="">
-              <h1 className="md:text-[60px] text-[50px]">
+              <h1 className="md:text-[60px] text-[30px]">
                 Need Creative Design?
               </h1>
-              <h2 className="flex md:text-[50px] text-[40px] md:mb-[30px]">
+              <h2 className="flex md:text-[50px] text-[30px] md:mb-[30px]">
                 I Can Help You!
               </h2>
-              <p className=" mb-5 md:text-[25px] text-[20px]">
+              <p className=" mb-5 md:text-[20px] md:leading-[60px] leading-[30px]">
                 I am the Designerable—a skilled designer with expertise in both
                 design and prototyping.My toolkit includes Figma and Adobe
                 Illustrator, and I possess a basic understanding of frontend
@@ -106,6 +120,19 @@ export default function Home() {
                 <img src={cv}></img>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="bg-[#000000] pt-10 w-[100%] rounded-2xl ">
+          <div className="text-center">
+            <span className="bg-gradient-to-r from-[#B16CEA] via-[#FF7460] to-[#FFA84B] bg-clip-text text-transparent text-3xl">
+              Selected Projects
+            </span>
+            <p className="text-white mt-3 md:text-[20px] flex justify-center text-center">
+              i can design nice and beautiful designs with either a mobile or
+              web interface. <br /> We also deal in graphics design and web
+              development.{" "}
+            </p>
           </div>
         </div>
       </div>
